2bfd52ae3d7c6aefe1b4505ab06a24a569a8bbee
piaolaidelangman <707056129@qq.com>
2022-02-02 18:34:56 +0800

    Yield for schema

--- src/main/scala/com/piaolaidelangman/xgbClassifierTrainingExample.scala
+++ src/main/scala/com/piaolaidelangman/xgbClassifierTrainingExample.scala
@@ -57,11 +57,11 @@ object xgbClassifierTrainingExample {
 
 
     val rowRDD = decryptionRDD.map(_.split(" ")).map(row => Row(
-      0 until row.length flatMap {
+      (0 until row.length flatMap {
       // case 0 => Some(row(0).toString)
       // case i if row(i) == null => None
       case i => Some( if (i < 14) row(i).toInt else row(i).toLong )
-    } toSeq
+    }).toArray
       // for{
       //   i <- 0 to 39
       // } yield {
