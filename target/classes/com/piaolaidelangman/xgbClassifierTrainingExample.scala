565e5488c016a1a2a174addf94c1bcf83e8ab012
piaolaidelangman <707056129@qq.com>
2022-02-02 18:54:00 +0800

    15

--- target/classes/com/piaolaidelangman/xgbClassifierTrainingExample.scala
+++ target/classes/com/piaolaidelangman/xgbClassifierTrainingExample.scala
@@ -64,8 +64,8 @@ object xgbClassifierTrainingExample {
       (for{
         i <- 0 to 39
       } yield {
-        // if(i<14) row(i).toInt else row(i).toLong
-        row(i).toLong
+        if(i<15) row(i).toInt else row(i).toLong
+        // row(i).toLong
       }).toSeq
     ))
 
