906a8c5b9a80a2b7f96792e5aaac3aa37bcd363f
piaolaidelangman <707056129@qq.com>
2022-02-05 12:56:56 +0800

    Update all

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -4,12 +4,12 @@ sudo docker run -it \
 	--privileged \
 	--net=host \
 	--name=xgboost-spark-sgx \
-	--cpuset-cpus 10-14 \
+	--cpuset-cpus 10-49 \
 	--device=/dev/sgx/enclave \
 	--device=/dev/sgx/provision \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
-    -v data:/opt/occlum_spark/data \
-	-e LOCAL_IP=Your_IP \
-	-e SGX_MEM_SIZE=24GB \
+    -v /home/sdp/diankun/encrypted_data_50G:/opt/occlum_spark/data \
+	-e LOCAL_IP=192.168.0.111 \
+	-e SGX_MEM_SIZE=100GB \
 	xgboost-spark-sgx:1.0 \
 	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
