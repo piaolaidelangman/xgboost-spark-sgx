d29a683c4ac13b9f1d35727ae3285109c46a7407
piaolaidelangman <707056129@qq.com>
2022-02-09 00:59:07 +0800

    Other success

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -16,9 +16,9 @@ init_instance() {
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
         .resource_limits.max_num_of_threads = 4096 |
-        .process.default_heap_size = "10240MB" |
+        .process.default_heap_size = "4096MB" |
         .resource_limits.kernel_space_heap_size="4096MB" |
-        .process.default_mmap_size = "204800MB" |
+        .process.default_mmap_size = "163840MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
@@ -88,6 +88,48 @@ run_spark_pi() {
                 --class org.apache.spark.examples.SparkPi spark-internal
 }
 
+# run_spark_xgboost_train() {
+#     init_instance spark
+#     build_spark
+#     echo -e "occlum run xgboost spark "
+#     occlum run /usr/lib/jvm/java-11-openjdk-amd64/bin/java \
+#                 -XX:-UseCompressedOops -XX:MaxMetaspaceSize=1024m \
+#                 -XX:ActiveProcessorCount=4 \
+#                 -Divy.home="/tmp/.ivy" \
+#                 -Dos.name="Linux" \
+#                 -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*:/bin/jars/*" \
+#                 -Xms196g org.apache.spark.deploy.SparkSubmit \
+#                 --master local[16] \
+#                 --conf spark.task.cpus=16 \
+#                 --class xgboostsparksgx.xgbClassifierTrainingExample \
+#                 --conf spark.scheduler.maxRegisteredResourcesWaitingTime=50000000 \
+#                 --conf spark.worker.timeout=60000000 \
+#                 --conf spark.network.timeout=10000000 \
+#                 --conf spark.starvation.timeout=2500000 \
+#                 --conf spark.speculation=false \
+#                 --conf spark.executor.heartbeatInterval=10000000 \
+#                 --conf spark.sql.shuffle.partitions=400 \
+#                 --conf spark.shuffle.io.maxRetries=8 \
+#                 --conf spark.driver.maxResultSize=8g \
+#                 --conf spark.driver.memoryOverhead=20g \
+#                 --conf spark.executor.memoryOverhead=2g \
+#                 --conf spark.kryoserializer.buffer.max=1024m \
+#                 --conf spark.memory.offHeap.enabled=true \
+#                 --conf spark.memory.offHeap.size=8g \
+#                 --conf spark.storage.memoryMapThreshold=8m \
+#                 --conf spark.task.reaper.threadDump=false \
+#                 --conf spark.sql.files.maxPartitionBytes=512m \
+#                 --conf spark.sql.inMemoryColumnarStorage.enableVectorizedReader=false \
+#                 --num-executors 16 \
+#                 --executor-cores 4 \
+#                 --executor-memory 8G \
+#                 --driver-memory 64G \
+#                 /bin/jars/xgboostsparksgx-1.0-SNAPSHOT-jar-with-dependencies.jar \
+#                 /host/data 16 1 /host/data/model LDlxjm0y3HdGFniIGviJnMJbmFI+lt3dfIVyPJm1YSY=
+
+#                 # --conf spark.executor.instances=16 \
+# }
+
 run_spark_xgboost_train() {
     init_instance spark
     build_spark
@@ -98,7 +140,7 @@ run_spark_xgboost_train() {
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
                 -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*:/bin/jars/*" \
-                -Xms196g org.apache.spark.deploy.SparkSubmit \
+                -Xmx128g -Xms128g org.apache.spark.deploy.SparkSubmit \
                 --master local[16] \
                 --conf spark.task.cpus=16 \
                 --class xgboostsparksgx.xgbClassifierTrainingExample \
@@ -108,26 +150,14 @@ run_spark_xgboost_train() {
                 --conf spark.starvation.timeout=2500000 \
                 --conf spark.speculation=false \
                 --conf spark.executor.heartbeatInterval=10000000 \
-                --conf spark.sql.shuffle.partitions=400 \
+                --conf spark.sql.shuffle.partitions=200 \
                 --conf spark.shuffle.io.maxRetries=8 \
-                --conf spark.driver.maxResultSize=8g \
-                --conf spark.driver.memoryOverhead=20g \
-                --conf spark.executor.memoryOverhead=2g \
-                --conf spark.kryoserializer.buffer.max=1024m \
-                --conf spark.memory.offHeap.enabled=true \
-                --conf spark.memory.offHeap.size=8g \
-                --conf spark.storage.memoryMapThreshold=8m \
-                --conf spark.task.reaper.threadDump=false \
-                --conf spark.sql.files.maxPartitionBytes=512m \
-                --conf spark.sql.inMemoryColumnarStorage.enableVectorizedReader=false \
-                --num-executors 16 \
-                --executor-cores 4 \
-                --executor-memory 8G \
-                --driver-memory 64G \
+                --num-executors 8 \
+                --executor-cores 2 \
+                --executor-memory 12G \
+                --driver-memory 30G \
                 /bin/jars/xgboostsparksgx-1.0-SNAPSHOT-jar-with-dependencies.jar \
                 /host/data 16 1 /host/data/model LDlxjm0y3HdGFniIGviJnMJbmFI+lt3dfIVyPJm1YSY=
-
-                # --conf spark.executor.instances=16 \
 }
 
 id=$([ -f "$pid" ] && echo $(wc -l < "$pid") || echo "0")
