1839be66b19851fccc92b7bcab39f8f9388494c4
piaolaidelangman <707056129@qq.com>
2022-02-10 16:42:32 +0800

    Delete missing in xgb conf

--- src/main/scala/com/piaolaidelangman/xgbClassifierTrainingExample.scala
+++ src/main/scala/com/piaolaidelangman/xgbClassifierTrainingExample.scala
@@ -78,7 +78,6 @@ object xgbClassifierTrainingExample {
 
     val xgbParam = Map("tracker_conf" -> TrackerConf(0L, "scala"),
       "eval_sets" -> Map("eval1" -> eval1, "eval2" -> eval2),
-      "missing" -> 0.0,
       "use_external_memory" -> true,
       "allow_non_zero_for_missing" ->true,
       "cache_training_set" -> true,
