403a581159b62a1430daca19feea8defd3bb9fee
piaolaidelangman <707056129@qq.com>
2022-02-11 14:46:27 +0800

    50g heap 90g epc

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -16,7 +16,7 @@ init_instance() {
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
         .resource_limits.max_num_of_threads = 4096 |
-        .process.default_heap_size = "40960MB" |
+        .process.default_heap_size = "51200MB" |
         .resource_limits.kernel_space_heap_size="2048MB" |
         .process.default_mmap_size = "32768MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
