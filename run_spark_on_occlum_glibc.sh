945bf08b3df0dbc75fc478b86e1c4013c7cf7766
piaolaidelangman <707056129@qq.com>
2022-02-11 13:29:58 +0800

    40g heap 80g epc

--- run_spark_on_occlum_glibc.sh
+++ run_spark_on_occlum_glibc.sh
@@ -16,9 +16,9 @@ init_instance() {
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
         .resource_limits.max_num_of_threads = 4096 |
-        .process.default_heap_size = "36864MB" |
+        .process.default_heap_size = "40960MB" |
         .resource_limits.kernel_space_heap_size="2048MB" |
-        .process.default_mmap_size = "40960MB" |
+        .process.default_mmap_size = "32768MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
@@ -98,7 +98,7 @@ run_spark_xgboost_train() {
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
                 -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*:/bin/jars/*" \
-                -Xmx36g -Xms36g org.apache.spark.deploy.SparkSubmit \
+                -Xmx30g -Xms30g org.apache.spark.deploy.SparkSubmit \
                 --master local[16] \
                 --conf spark.task.cpus=4 \
                 --conf spark.task.maxFailures=8 \
@@ -109,10 +109,10 @@ run_spark_xgboost_train() {
                 --conf spark.starvation.timeout=2500000 \
                 --conf spark.executor.heartbeatInterval=10000000 \
                 --conf spark.shuffle.io.maxRetries=8 \
-                --num-executors 2 \
-                --executor-cores 8 \
-                --executor-memory 8G \
-                --driver-memory 16G \
+                --num-executors 8 \
+                --executor-cores 2 \
+                --executor-memory 2G \
+                --driver-memory 10G \
                 /bin/jars/xgboostsparksgx-1.0-SNAPSHOT-jar-with-dependencies.jar \
                 /host/data 2 /host/data/model LDlxjm0y3HdGFniIGviJnMJbmFI+lt3dfIVyPJm1YSY=
 }
