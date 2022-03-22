913b8fd35f663a1b78aff549866b113df03ca6a7
piaolaidelangman <707056129@qq.com>
2022-03-22 22:30:03 +0800

    data

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -8,8 +8,8 @@ sudo docker run -it \
 	--device=/dev/sgx/enclave \
 	--device=/dev/sgx/provision \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
-    -v data/:/opt/occlum_spark/data \
-	-e LOCAL_IP=LOCAL_IP \
+	-v data:/opt/occlum_spark/data \
+	-e LOCAL_IP=$LOCAL_IP \
 	-e SGX_MEM_SIZE=24GB \
 	-e SGX_THREAD=512 \
 	-e SGX_HEAP=512MB \
