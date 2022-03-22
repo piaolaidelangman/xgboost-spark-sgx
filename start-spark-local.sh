1ca6fd1b02715db935f48f84ed20986f76724247
piaolaidelangman <707056129@qq.com>
2022-03-22 21:05:54 +0800

    for spark pi envs

--- start-spark-local.sh
+++ start-spark-local.sh
@@ -10,10 +10,10 @@ sudo docker run -it \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
     -v data/:/opt/occlum_spark/data \
 	-e LOCAL_IP=LOCAL_IP \
-	-e SGX_MEM_SIZE=40GB \
+	-e SGX_MEM_SIZE=24GB \
 	-e SGX_THREAD=512 \
 	-e SGX_HEAP=512MB \
 	-e SGX_KERNEL_HEAP=1GB \
-	-e SGX_MMAP=36GB \
+	-e SGX_MMAP=16GB \
 	xgboost-spark-sgx:1.0.0 \
 	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
