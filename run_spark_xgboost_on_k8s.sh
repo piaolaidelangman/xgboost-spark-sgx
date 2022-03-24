c0d489f81e5a346c45cde8a77cf0ac69d505ad40
piaolaidelangman <707056129@qq.com>
2022-03-24 12:46:29 +0800

    update scripts

--- run_spark_xgboost_on_k8s.sh
+++ run_spark_xgboost_on_k8s.sh
@@ -5,14 +5,14 @@ ${SPARK_HOME}/bin/spark-submit \
     --deploy-mode cluster \
     --name spark-xgboost-sgx \
     --class xgboostsparksgx.xgbClassifierTrainingExample \
-    --conf spark.executor.instances=1 \
     --conf spark.rpc.netty.dispatcher.numThreads=32 \
-    --conf spark.kubernetes.container.image=xgboost-spark-sgx \
+    --conf spark.kubernetes.container.image=xgboost-spark-sgx:1.0.0 \
     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
     --conf spark.kubernetes.executor.deleteOnTermination=false \
     --conf spark.kubernetes.driver.podTemplateFile=./executor.yaml \
     --conf spark.kubernetes.executor.podTemplateFile=./executor.yaml \
     --conf spark.kubernetes.file.upload.path=file:///tmp \
+    --conf spark.executorEnv.SGX_MEM = 16GB \
     --conf spark.task.cpus=2 \
     --num-executors 2 \
     --executor-cores 4 \
